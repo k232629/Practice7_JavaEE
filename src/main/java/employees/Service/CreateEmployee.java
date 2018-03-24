0dbf35b85475d3ddc666b3cf9865a143595c2c5f
k232629 <olhakaraiman@gmail.com>
2018-03-24 23:40:42 +0200

    Checking cache

--- src/main/java/employees/Service/CreateEmployee.java
+++ src/main/java/employees/Service/CreateEmployee.java
@@ -5,7 +5,10 @@ import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.Persistence;
 import employees.Entity.Employee;
+import org.springframework.cache.annotation.EnableCaching;
 
+
+@EnableCaching
 public class CreateEmployee {
 
     public static void main( String[ ] args ) {
@@ -16,22 +19,24 @@ public class CreateEmployee {
         entitymanager.getTransaction( ).begin( );
 
         Employee employee = new Employee( );
-        employee.setEid( 1207 );
+        employee.setEid( 4446 );
         employee.setEname( "Gopal" );
         employee.setSalary( 40000 );
         employee.setDeg( "Technical Manager" );
 
-        Employee employee3 = new Employee( );
-        employee3.setEid( 1208 );
-        employee3.setEname( "Satish" );
-        employee3.setSalary( 400000 );
-        employee3.setDeg( "Technical Writer" );
 
 
         entitymanager.persist( employee );
         entitymanager.getTransaction( ).commit( );
         Cache cache = emfactory.getCache();
         assert(cache.contains(Employee.class,employee.getEid()));
+
+        employee.getEid();
+        System.out.println("first time");
+        assert(cache.contains(Employee.class,employee.getEid()));
+        employee.getEid();
+
+
 
 
         entitymanager.close( );
